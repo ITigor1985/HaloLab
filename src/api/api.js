abfbfb171e29de71c79f0835821287805482831a
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-06-09 20:57:37 +0300

    fix

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const getCards = async () => {
-  axios.defaults.baseURL = `http://localhost:4000/`;
-  //axios.defaults.baseURL = `https://run.mocky.io/v3/b7d36eea-0b3f-414a-ba44-711b5f5e528e`;
+  //axios.defaults.baseURL = `http://localhost:4000/`;
+  axios.defaults.baseURL = `https://run.mocky.io/v3/b7d36eea-0b3f-414a-ba44-711b5f5e528e`;
   const response = await axios.get(`${axios.defaults.baseURL}`);
   return response.data;
 };
