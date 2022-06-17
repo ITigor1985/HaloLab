f5f883991574d3c3f18c796506e634183a60d048
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-06-17 23:09:21 +0300

    add fetch

--- src/api/api.js
+++ src/api/api.js
@@ -1,16 +1,31 @@
-import axios from 'axios';
+//import axios from 'axios';
 
 export const getCards = async () => {
   //axios.defaults.baseURL = `http://localhost:4000/`;
-  axios.defaults.baseURL = `https://run.mocky.io/v3/b7d36eea-0b3f-414a-ba44-711b5f5e528e`;
-  const response = await axios.get(`${axios.defaults.baseURL}`);
-  return response.data;
+  //axios.defaults.baseURL = `https://run.mocky.io/v3/b7d36eea-0b3f-414a-ba44-711b5f5e528e`;
+  let url = `https://run.mocky.io/v3/b7d36eea-0b3f-414a-ba44-711b5f5e528e`;
+  //const response = await axios.get(`${axios.defaults.baseURL}`);
+  const response = await fetch(url);
+  if (response.ok) {
+    let json = await response.json();
+    return json;
+  } else {
+    return response.status;
+  }
+  // return response.data;
 };
 
 export const postContact = async item => {
   try {
-    const response = await axios.post('http://localhost:4000/', item);
-    console.log('👉 Returned data:', response);
+    const response = await fetch('http://localhost:4000/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify(item),
+    });
+    //const response = await axios.post('http://localhost:4000/', item);
+    console.log('👉 Returned data:', response.ok);
   } catch (e) {
     console.log(`😱 Axios request failed: ${e}`);
   }
